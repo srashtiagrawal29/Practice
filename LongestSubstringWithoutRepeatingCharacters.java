675d81f4eef249e22029d4676c5b5e36b2195ac8
Srashti Agrawal <76569512+srashtiagrawal29@users.noreply.github.com>
2021-03-09 16:21:40 +0530

    Add files via upload

---
+++ LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,15 @@
+Problem Statement: Given a string s, find the length of the longest substring without repeating characters.
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        char arr[]=s.toCharArray();
+        int count=1;
+        
+        Stack s1<Charachter>=new Stack();
+        
+        return 0;
+    }
+}
